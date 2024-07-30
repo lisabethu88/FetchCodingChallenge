827f735f074cdf006e37c39041e9a722d278a5b0
Lisa Utsett <lisabethutsett@gmail.com>
2024-07-30 15:21:55 -0400

    Initial Commit

---
+++ FetchCodingChallenge/FetchCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchCodingChallengeApp.swift
+//  FetchCodingChallenge
+//
+//  Created by Lisa Utsett on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
